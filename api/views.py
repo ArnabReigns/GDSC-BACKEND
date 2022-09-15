68d56f5665c17121e7df241dede43f548f2a9847
ArnabReigns <tysonchatterjee@gmail.com>
2022-09-16 02:45:43 +0530

    2nd commit

---
+++ api/views.py
@@ -0,0 +1,7 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# Create your views here.
+
+@api_view(['GET'])
+def home(req):
+    return Response('Welcome To the API')
